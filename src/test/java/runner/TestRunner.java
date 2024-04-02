3f918756449699884abfd76978d9b923a15eff8a
ShwetaSaini1234 <147542684+ShwetaSaini1234@users.noreply.github.com>
2024-04-01 19:51:53 -0500

    Corrected TestRunner

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -7,15 +7,16 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        features = "src/test/resources/Features/",
+        features="src/test/resources/features",
         glue = {"com.lms.api.stepdefs"},
-       // tags = "@EndtoEnd",
-        monochrome = true,
-        plugin = {"pretty",
-                "html:target/cucumber-html-report", "json:target/cucumber.json",
-                "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
-                "pretty","io.qameta.allure.cucumber7jvm.AllureCucumber7"
-                        + "Jvm"}
+        		monochrome = true,
+                plugin = {"pretty",
+                        "html:target/cucumber-html-report", "json:target/cucumber.json",
+                        "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
+                        "pretty","io.qameta.allure.cucumber7jvm.AllureCucumber7"
+                                + "Jvm"}
 )
 public class TestRunner {
 }
+	
+	
